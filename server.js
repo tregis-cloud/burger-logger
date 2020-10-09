9336cbc91df7f4969929a732c2e773fb2e5529e6
Terrence Regis <terrence.regis@gmail.com>
2020-10-08 21:52:51 -0400

    basic server.js setup.

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+app.get("/api/config", function (req, res){
+    res.json({ success: true,
+    });
+});
+// var routes = require("./controllers/catsController.js");
+
+// app.use(routes);
+
+// Start server 
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
