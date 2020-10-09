883f3c6d1c988180dec766a04a50892ffe884d1a
Terrence Regis <terrence.regis@gmail.com>
2020-10-09 00:17:02 -0400

    Added MVC

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+const router = express.Router();
+
+const burger = require("../models/burger.js");
+
+// Routes
+router.get("/", function(req, res) {
+  cat.all(function(data) {
+    let hbsObject = {
+      burger: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burger", function(req, res) {
+  burger.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.name, req.body.sleepy
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burger/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+
+
+module.exports = router;
