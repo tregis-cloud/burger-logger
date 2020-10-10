648d2e042fda1628952b024f41c1b8c2f5eff499
Terrence Regis <terrence.regis@gmail.com>
2020-10-09 22:01:00 -0400

    Updated README and fixed bugs.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -30,9 +30,11 @@ router.put("/api/burger/:id", function(req, res) {
   let condition = "id = " + req.params.id;
 
   console.log("condition", condition);
+ 
 
   burger.update({
-    devoured: req.body.devoured
+   
+    devoured: 1
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
